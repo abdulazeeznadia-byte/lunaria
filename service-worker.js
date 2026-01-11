6b889b4db7b4e0083ba46c3259a1f6065751626c
abdulazeeznadia-byte <abdulazeeznadia@gmail.com>
2026-01-11 20:20:48 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install",e=>{
+  e.waitUntil(
+    caches.open("lunaria").then(c=>c.addAll(["./","index.html"]))
+  );
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
